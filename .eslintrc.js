4e2cee9000f41573547d8775c139a80e19ad7287
sonu yadav <mr.sonusangam@gmail.com>
2023-07-18 19:37:17 +0530

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:import/recommended",
+        "plugin:import/errors",
+        "plugin:import/warnings"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+        // .eslintrc.js
+        "plugins": ['eslint-plugin-import-helpers',"import"],
+        "rules": {
+            "import/extensions": "off",
+        }
+}
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box ,Grid} from '@mui/material';
+import LandingPage from './components/ui/landing_page/landing_page';
+import StepperComponent from "./components/ui/others/stepper";
+
+export default function App() {
+  return (
+   <Box p={4}>
+     <Grid container direction="column" justifyContent="center">
+      <Grid item>
+        <LandingPage/>
+      </Grid>
+      {/* Stepper for reference */}
+      <Grid item>
+        <StepperComponent/>
+      </Grid>
+    </Grid>
+   </Box>
+  );
+}
